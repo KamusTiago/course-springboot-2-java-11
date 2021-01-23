73a14993527a92e85c10146277ba5c30d05fe738
KamusTiago <tiagokamus40@gmail.com>
2021-01-23 19:56:26 -0300

    banco de dados H2, Teste profile, JPA

--- src/main/java/com/kamustiago/course/entities/User.java
+++ src/main/java/com/kamustiago/course/entities/User.java
@@ -2,11 +2,19 @@ package com.kamustiago.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 //se utiliza o Serializible para transformar os objetos 
 //em cadeia de bytes para que trafeguem na rede ou gravados em arquivos. "implements Serializable"
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
