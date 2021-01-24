f51dc984b86adc6c412142e2198c56ea9ecc895c
KamusTiago <tiagokamus40@gmail.com>
2021-01-24 12:15:57 -0300

    Delecao de User

--- src/main/java/com/kamustiago/course/services/UserService.java
+++ src/main/java/com/kamustiago/course/services/UserService.java
@@ -11,21 +11,25 @@ import com.kamustiago.course.repositories.UserRepository;
 //Registrando a classe como componente do spring
 @Service
 public class UserService {
-	//injeção de dependencia
+	// injeção de dependencia
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		java.util.Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 	
+	public void delet(Long id) {
+		repository.deleteById(id);
+	}
+
 }
