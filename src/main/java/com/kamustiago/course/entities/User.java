128f082c844e155fe53babd81298628e668baf31
KamusTiago <tiagokamus40@gmail.com>
2021-01-23 21:33:10 -0300

    Entidade pedido, datas com Instant e padrao ISO 8601 - parte 1

--- src/main/java/com/kamustiago/course/entities/User.java
+++ src/main/java/com/kamustiago/course/entities/User.java
@@ -1,15 +1,20 @@
 package com.kamustiago.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 //se utiliza o Serializible para transformar os objetos 
 //em cadeia de bytes para que trafeguem na rede ou gravados em arquivos. "implements Serializable"
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -20,6 +25,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String passWord;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User(){
 		
@@ -71,6 +79,10 @@ public class User implements Serializable{
 
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
